ff3ee81a807b5901bcdb690ec636636190d56947
Juliano Martins de Souza <JulianoMarthins@Yahoo.com.br>
2023-11-11 18:28:47 -0300

    ver02_11/11

--- Testes.py
+++ Testes.py
@@ -1,38 +1,5 @@
 print('\n')
  
-vendas = [
-    ['maçã', 5],
-    ['banana', 15],
-    ['azeite', 1],
-    ['vinho', 3],
-]
 
-produtos = []
-
-while True:
-
-    nome = input('Nome do produto: ')
-
-    if not nome:
-        print('Programa será fechado')
-        break
-    
-    quantidade = input('Unidades: ')
- 
-    produtos.append(nome)
-    produtos.append(quantidade)
-
-    vendas.append(produtos)
-    produtos.clear()
-
-print('Lista de produtos')
-
-
-for prod in vendas:
-
-    nome = prod[0]
-    quantidade = prod[1]
-
-    print(f'Produto: {nome} {quantidade} unds')
 
 print('\n')
