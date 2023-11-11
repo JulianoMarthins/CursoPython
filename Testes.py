53c0e6b5d1f471f563370b25bf1955bf89422a85
Juliano Martins de Souza <JulianoMarthins@Yahoo.com.br>
2023-11-10 21:26:54 -0300

    ver_3 10/11/2023

--- Testes.py
+++ Testes.py
@@ -1,10 +1,14 @@
 print('\n')
 
-nome = 'Juliano Martins de Souza'
-
-teste = '-'.join(nome)
-
-print(teste)
-
+while True:
+    estado_civil = input(\
+        'Digite seu estado civil:\n[S]olteiro\n[C]asado\n[V]iuvo\n[D]disquitado'\
+            ).upper()
+    
+    if 'SCVD' not in estado_civil:
+        print('Naõ achei')
+    else:
+        print('achei')
+  
 
 print('\n')
