e0264c649a45088ba3a3c0b85828a2447e5c981c
Juliano Martins de Souza <99154908+JulianoMarthins@users.noreply.github.com>
2023-11-16 19:10:03 -0300

    ver002_16/11/2023

---
+++ Modulo_02/04_Listas_Dentro_Listas.py
@@ -0,0 +1,14 @@
+"""
+        Metodos de trabalho com listas dentro de outra lista
+        
+"""
+
+vendedores = ['Juliano', 'Lira', 'Thiele', 'Alon']
+produtos = ['ipad', 'iphone']
+
+vendas = [
+    [100, 200],
+    [300, 500],
+    [50, 1000],
+    [900, 10]
+]
