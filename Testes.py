9a83fd87e183fe289c5c785f0b9e350c6754ebb2
Juliano Martins de Souza <JulianoMarthins@Yahoo.com.br>
2023-11-19 21:48:17 -0300

    Ver01_19/11/2023

--- Testes.py
+++ Testes.py
@@ -1,3 +1,13 @@
-nome = 'JULiano'
+print()
 
-nome = nome.upper()
+
+lista = [('juliano', 321, 14), ('thieles', 65748, 31)]
+
+
+for indice in lista:
+    cpf, x, y = indice
+    
+    if x > 300:
+        print(cpf)
+
+print(type(lista))
