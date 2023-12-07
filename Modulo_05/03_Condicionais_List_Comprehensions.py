16e5534814dd19724f996aba8b5bcb4228abc1e8
Juliano Martins de Souza <99154908+JulianoMarthins@users.noreply.github.com>
2023-12-07 17:40:43 -0300

    Finalização de capitulo list Comprehesions

--- Modulo_05/03_Condicionais_List_Comprehensions.py
+++ Modulo_05/03_Condicionais_List_Comprehensions.py
@@ -1,4 +1,18 @@
 """
-        Você pode adicionar condições no meio do list Comprehensions
+        Você pode adicionar condições no meio do list Comprehensions, a condição deve ficar ao
+    final do código
 
 """
+
+meta = 1000
+venda = [1500, 150, 2100, 1950]
+produto = ['vinho', 'cafeteira', 'microondas', 'iphone']
+
+# Na linha de código abaixo, examinamos se o número de produtos vendidos na lista vendas é
+# superior a 1000, retornando True, adicionamos a variavel acima_media o nome do produto que 
+# está no mesmo indice da variavel vendas, obtendo assim, o nome do produto correspondente
+# aquela vaga.
+
+acima_media = [produto for i, produto in enumerate(produto) if venda[i] > meta]
+
+print('Produtos acima da média:\n{}'.format(acima_media))
