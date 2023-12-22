d5d8d8da99c0dd2913139c04c31b8e273cce44ae
Juliano Martins de Souza <JulianoMarthins@Yahoo.com.br>
2023-12-21 23:08:39 -0300

    ver01_21/12

--- Testes.py
+++ Testes.py
@@ -1,14 +1,28 @@
 import pandas as pd
 
-produtos_df = pd.read_csv(
-    r'D:\\WorkSpace\\Python\\Python_Impressionador\\Pandas\\Contoso - Cadastro Produtos.csv', sep=';', encoding='cp1252'
-    )
+# Lendo os arquivos CSV da empresa
+vendas_df = pd.read_csv(r'Contoso - Vendas - 2017.csv', sep=';')
+produtos_df = pd.read_csv(r'Contoso - Cadastro Produtos.csv', sep=';', encoding='ISO8859-1')
+lojas_df = pd.read_csv(r'Contoso - Lojas.csv', sep=';', encoding='ISO8859-1')
+cliente_df = pd.read_csv(r'Contoso - Clientes.csv', sep=';', encoding='ISO8859-1')
 
-print(produtos_df)
+# Renomeando colunas para padronizar as tabelas
+produtos_df = produtos_df.rename(columns={'ÿNome do Produto': 'Nome Produto'})
+lojas_df = lojas_df.rename(columns={'ÿID Loja': 'ID Loja'})
+cliente_df = cliente_df.rename(columns={'ÿID Cliente': "ID Cliente"})
+cliente_df = cliente_df.rename(columns={'E-mail': 'E-mail do Cliente'})
 
-"""
-* encoding='Latin1'
-            * encoding='ISO-8859-1'
-            * encoding='utf-8'
-            * encoding='cp1252'
-            """
+
+# Filtrando colunas necessárias a tabela
+cliente_df = cliente_df[['ID Cliente', 'E-mail do Cliente']]
+produtos_df = produtos_df[['ID Produto', 'Nome Produto']]
+lojas_df = lojas_df[['ID Loja', 'Nome da Loja']]
+
+# Associando as tabelas pela ID
+vendas_df = vendas_df.merge(produtos_df, on='ID Produto')
+vendas_df = vendas_df.merge(lojas_df, on='ID Loja')
+vendas_df = vendas_df.merge(cliente_df, on='ID Cliente')
+
+vendas_lojas = vendas_df.groupby('Nome da Loja').sum()
+vendas_loja = vendas_lojas[['Quantidade Vendida']]                                                            
+print(vendas_lojas)
